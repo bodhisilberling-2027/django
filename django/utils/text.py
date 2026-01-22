2077e6de51b5bb14caca06d1d5c75cfed780733b
Bodhi Silberling <bodhirussellsilberling@yahoo.com>
2026-01-21 16:37:19 -0800

    Modernize string formatting in get_valid_filename
    Replace old-style % formatting with f-string for consistency with
    the rest of the file.

--- django/utils/text.py
+++ django/utils/text.py
@@ -279,7 +279,7 @@ def get_valid_filename(name):
     s = str(name).strip().replace(" ", "_")
     s = re.sub(r"(?u)[^-\w.]", "", s)
     if s in {"", ".", ".."}:
-        raise SuspiciousFileOperation("Could not derive file name from '%s'" % name)
+        raise SuspiciousFileOperation(f"Could not derive file name from '{name}'")
     return s
 
 
